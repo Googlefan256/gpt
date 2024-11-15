de4e2b6563f31c9f5f366bbc8c2cee48ff667e5b
Googlefan <google@neody.dev>
2024-11-15 11:00:47 +0000

    wip: sft

---
+++ infer-chat.py
@@ -0,0 +1,52 @@
+from transformers import (
+    AutoTokenizer,
+    AutoModelForCausalLM,
+    GenerationConfig,
+    TextIteratorStreamer,
+)
+import torch
+from threading import Thread
+
+device = "cuda:0"
+tokenizer = AutoTokenizer.from_pretrained("./ckpt/tokenizer")
+model = AutoModelForCausalLM.from_pretrained(
+    "neody/nemma-100m",
+    attn_implementation="sdpa",
+    torch_dtype=torch.bfloat16,
+    device_map=device,
+)
+tokenizer.add_tokens(["<|start_of_turn|>", "<|end_of_turn|>"])
+model.resize_token_embeddings(len(tokenizer))
+model.load_adapter("./ckpt/checkpoint-46000")
+model = torch.compile(
+    model, options={"triton.cudagraphs": True}, fullgraph=True, dynamic=True
+)
+with torch.inference_mode():
+    while True:
+        streamer = TextIteratorStreamer(
+            tokenizer=tokenizer, skip_prompt=True, skip_special_tokens=True
+        )
+        kwargs = dict(
+            **tokenizer(
+                tokenizer.apply_chat_template(
+                    [{"role": "user", "content": input("Message: ")}],
+                    add_generation_prompt=True,
+                    tokenize=False,
+                ),
+                return_tensors="pt",
+            ).to(device),
+            generation_config=GenerationConfig(
+                max_new_tokens=2000,
+                repetition_penalty=1.2,
+                num_beams=1,
+                do_sample=True,
+                eos_token_id=tokenizer.eos_token_id,
+            ),
+            streamer=streamer,
+        )
+        thread = Thread(target=model.generate, kwargs=kwargs)
+        print("Output: ", end="", flush=True)
+        thread.start()
+        for text in streamer:
+            print(text, end="", flush=True)
+        print()
---
+++ trains/sft.py
@@ -0,0 +1,97 @@
+from datasets import load_dataset, Dataset
+import torch
+from transformers import (
+    Gemma2ForCausalLM,
+    GPT2TokenizerFast,
+)
+from trl import SFTTrainer, SFTConfig
+from peft import LoraConfig, TaskType
+
+torch.backends.cuda.matmul.allow_tf32 = True
+torch.set_float32_matmul_precision("high")
+torch.backends.cudnn.allow_tf32 = True
+torch.backends.cudnn.benchmark = True
+torch.backends.cuda.enable_flash_sdp(True)
+
+if __name__ == "__main__":
+    max_seq_len = 3096
+    model: Gemma2ForCausalLM = Gemma2ForCausalLM.from_pretrained(
+        "neody/nemma-100m",
+        attn_implementation="sdpa",
+        torch_dtype=torch.bfloat16,
+        device_map="cuda",
+    )
+    tokenizer: GPT2TokenizerFast = GPT2TokenizerFast.from_pretrained("neody/nemma-100m")
+    tokenizer.add_tokens(["<|start_of_turn|>", "<|end_of_turn|>"])
+    model.resize_token_embeddings(len(tokenizer))
+    peft_config = LoraConfig(
+        task_type=TaskType.CAUSAL_LM,
+        inference_mode=False,
+        r=8,
+        lora_alpha=32,
+        lora_dropout=0.1,
+        target_modules=[
+            "q_proj",
+            "k_proj",
+            "v_proj",
+            "o_proj",
+            "w1",
+            "w2",
+            "w3",
+        ],
+    )
+    with open("./template.jinja", "r") as r:
+        tokenizer.chat_template = r.read()
+
+    def formatting(example):
+        conv = example["conversations"]
+        text = tokenizer.apply_chat_template(
+            [
+                [
+                    {
+                        "role": "user" if y["from"] == "human" else "assistant",
+                        "content": y["value"],
+                    }
+                    for y in x
+                ]
+                for x in conv
+            ],
+            tokenize=False,
+        )
+        return {"text": text}
+
+    ds: Dataset = load_dataset("BAAI/Infinity-Instruct", "0625", split="train")
+    ds = ds.map(formatting, batched=True, remove_columns=ds.column_names, num_proc=6)
+    ds_len = len(ds)
+    ds = ds.to_iterable_dataset()
+    tokenizer.padding_side = "right"
+    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.save_pretrained("./ckpt/tokenizer")
+    training_args = SFTConfig(
+        optim="adamw_8bit",
+        output_dir="./ckpt",
+        logging_steps=5,
+        do_train=True,
+        per_device_train_batch_size=1,
+        gradient_accumulation_steps=8,
+        save_steps=1000,
+        save_total_limit=2,
+        prediction_loss_only=True,
+        max_seq_length=max_seq_len,
+        max_steps=ds_len // 4,
+        bf16=True,
+        learning_rate=6e-5,
+        weight_decay=0.1,
+        lr_scheduler_type="cosine",
+        warmup_ratio=0.1,
+        dataloader_num_workers=4,
+        dataset_text_field="text",
+    )
+    trainer = SFTTrainer(
+        model=model,
+        args=training_args,
+        train_dataset=ds,
+        tokenizer=tokenizer,
+        peft_config=peft_config,
+    )
+    trainer.train()
