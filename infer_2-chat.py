130681d0d135e7bdfabcca1cb9b376656fdf8a78
Googlefan <google@neody.dev>
2024-11-25 02:28:33 +0000

    fix

--- infer_2-chat.py
+++ infer_2-chat.py
@@ -1,12 +1,12 @@
 from trains.train_2_model import GPT, GPTConfig, CastedLinear
 from transformers import GPT2TokenizerFast, TextIteratorStreamer
 import torch
-from threading import Thread
 from huggingface_hub import hf_hub_download
+from threading import Thread
 
 device = "cuda:0"
 tokenizer: GPT2TokenizerFast = GPT2TokenizerFast.from_pretrained(
-    "openai-community/gpt2"
+    "neody/npt-100m-it", subfolder="tokenizer"
 )
 model = GPT(
     GPTConfig(
@@ -21,7 +21,7 @@ for m in model.modules():
     if isinstance(m, CastedLinear):
         m.float()
 model.load_state_dict(
-    torch.load(hf_hub_download("neody/npt-100m", "ckpt.pt"), weights_only=True)
+    torch.load(hf_hub_download("neody/npt-100m-it", "ckpt.pt"), weights_only=True)
 )
 if __name__ == "__main__":
     model: GPT = torch.compile(
@@ -35,18 +35,24 @@ if __name__ == "__main__":
             )
             kwargs = dict(
                 idx=tokenizer(
-                    msg,
+                    tokenizer.apply_chat_template(
+                        [
+                            {"role": "user", "content": msg},
+                        ],
+                        add_generation_prompt=True,
+                        tokenize=False,
+                    ),
                     return_tensors="pt",
                     add_special_tokens=True,
                 ).input_ids.to(device),
                 max_new_tokens=2000,
                 temperature=0.4,
                 repeat_penalty=1.2,
-                eos=tokenizer.eos_token_id,
+                eos=tokenizer.convert_tokens_to_ids("<|end_of_turn|>"),
                 streamer=streamer,
             )
             thread = Thread(target=model.generate, kwargs=kwargs)
-            print(f"Output: {msg}", end="", flush=True)
+            print(f"Output: ", end="", flush=True)
             thread.start()
             for x in streamer:
                 print(x, end="", flush=True)
